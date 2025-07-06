a0addc4cc75cffed3193d58af807839f32e11e3a
jwaria <fatimajwaria@gmail.com>
2025-07-06 12:29:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Hey there, welcome to Jwaria's world of code and creativity! 💻✨");
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.title = "Jwaria Fatima | Portfolio";
+});
